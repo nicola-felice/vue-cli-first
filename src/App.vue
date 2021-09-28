7ac4d1bc893a2f101532f5217eeb833006befff1
nicolafelice <felicenicola99@hotmail.com>
2021-09-28 17:00:40 +0200

    add: bonus

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <myTitle id="title"/>
+    <Hero/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import myTitle from './components/myTitle.vue';
+import Hero from './components/Hero.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    myTitle,
+    Hero
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  #title {
+    margin: 2rem 0;
+  }
 </style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="hero">
+    <h1 id="hero_title">Hero Title</h1>
+    <button id="hero_button">cliccami!</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Hero'
+  }
+</script>
+
+<style scoped lang="scss">
+  #hero_title {
+    color: white;
+    font-size: 5rem;
+  }
+  #hero_button {
+    background-color: red;
+    border: transparent;
+    border-radius: .9rem;
+    padding: .5rem 1rem;
+    cursor: pointer;
+    color: white;
+    font-size: 1rem;
+    margin-top: 1rem;
+  }
+  #hero {
+    background-image: url('../assets/jack-anstey-XVoyX7l9ocY-unsplash.jpg');
+    background-position: center;
+    background-size: cover;
+
+    padding: 12rem 4rem;
+    text-align: left;
+  }
+</style>
